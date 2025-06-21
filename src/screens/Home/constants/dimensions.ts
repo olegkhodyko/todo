a8e988768c82dccb54c91cb9383d01d389950490
Oleg Khodyko <olegkhodyko@Olegs-MacBook-Air.local>
2025-06-21 14:19:12 +0300

    Remove all comments from src folder

--- src/screens/Home/constants/dimensions.ts
+++ src/screens/Home/constants/dimensions.ts
@@ -1,4 +1,4 @@
-export const TASK_ITEM_HEIGHT = 120; // Высота айтема задачи в пикселях
+export const TASK_ITEM_HEIGHT = 120;
 export const TASK_ITEM_GAP = 8;
-export const TASK_ITEM_PADDING = 16; // Внутренний отступ айтема
-export const TASK_ITEM_BORDER_RADIUS = 8; // Радиус скругления айтема
+export const TASK_ITEM_PADDING = 16;
+export const TASK_ITEM_BORDER_RADIUS = 8;
